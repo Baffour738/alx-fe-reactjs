c9379344c389cdc525bd954beedada78cc119952
Akowuah Addo Baffour <Akowuah Addo Baffour>
2025-10-08 05:16:02 +0000

    Updated

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,19 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
 import useRecipeStore from "./recipeStore";
 
 const RecipeList = () => {
-  const { filteredRecipes, recipes } = useRecipeStore();
+  const { filteredRecipes, recipes, searchTerm, filterRecipes } = useRecipeStore();
 
-  const displayRecipes =
-    filteredRecipes.length > 0 ? filteredRecipes : recipes;
+  const displayedRecipes = searchTerm ? filteredRecipes : recipes;
 
   return (
     <div>
-      {displayRecipes.map((recipe) => (
-        <div key={recipe.id}>
-          <h3>{recipe.title}</h3>
-          <p>{recipe.description}</p>
-        </div>
-      ))}
+      <h2>Recipe List</h2>
+      {displayedRecipes.length === 0 ? (
+        <p>No recipes found.</p>
+      ) : (
+        <ul>
+          {displayedRecipes.map((recipe) => (
+            <li key={recipe.id}>
+              <Link to={`/recipe/${recipe.id}`}>{recipe.title}</Link>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
