8f1c12d361fe9185809f90ee14d178c77656abc5
Akowuah Addo Baffour <Akowuah Addo Baffour>
2025-10-10 00:58:27 +0000

    Added github-search

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", padding: "2rem" }}>
+      <h1>GitHub User Search</h1>
+      <p>Start building your app here...</p>
+    </div>
+  );
+}
+
+export default App;
